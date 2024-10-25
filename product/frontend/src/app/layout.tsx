a7e654f0cca10afd4e5061b2babf406eba361ecc
zlac370 <zlac370@live.rhul.ac.uk>
2024-10-25 13:40:14 +0100

    feat: added the apollo wrapper to the home layout and added and added a new example to the env

--- product/frontend/src/app/layout.tsx
+++ product/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
+import { ApolloWrapper } from "@/components/apollo";
 import { type Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,12 +11,18 @@ export const metadata: Metadata = {
   description: "Final Year Project",
 };
 
+const BACKEND_GRAPHQL_ENDPOINT = process.env.BACKEND_GRAPHQL_ENDPOINT ?? "";
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <ApolloWrapper link={BACKEND_GRAPHQL_ENDPOINT}>
+          {children}
+        </ApolloWrapper>
+      </body>
     </html>
   );
 }
