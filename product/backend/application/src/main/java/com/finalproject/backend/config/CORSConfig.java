a37d5210c4c4b1a148835052b1001596315fbc2a
zlac370 <zlac370@live.rhul.ac.uk>
2024-10-25 13:19:29 +0100

    feat: added cors generic cors config to the backend

---
+++ product/backend/application/src/main/java/com/finalproject/backend/config/CORSConfig.java
@@ -0,0 +1,17 @@
+package com.finalproject.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@SuppressWarnings("PMD.AtLeastOneConstructor")
+public class CORSConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(final CorsRegistry registry) {
+		registry.addMapping("/**").allowedOriginPatterns("*")
+				.allowedMethods("GET", "POST", "PUT").maxAge(-1)
+				.allowedHeaders("*").allowCredentials(true);
+	}
+}
