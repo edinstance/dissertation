88262e1a4464cf55eb08958c787fb6fb4f61a86b
zlac370 <zlac370@live.rhul.ac.uk>
2024-10-25 13:41:49 +0100

    feat: created a query based on mock data and implemented it to test it works

--- product/frontend/src/app/page.tsx
+++ product/frontend/src/app/page.tsx
@@ -1,5 +1,32 @@
+"use client";
+
+import { GET_SHOWS_QUERY } from "@/lib/graphql";
+import { useQuery } from "@apollo/client";
+
 export default function HomePage() {
+  const { data: queryData, loading: queryLoading } = useQuery(GET_SHOWS_QUERY, {
+    onCompleted(data) {
+      console.log("account data", data);
+    },
+    onError(error) {
+      console.error("query error", error.message);
+    },
+  });
+
   return (
-    <main className="flex min-h-screen items-center justify-center text-white"></main>
+    <main className="flex min-h-screen items-center justify-center text-black">
+      <div className="flex flex-col">
+        <h1 className="text-4xl font-bold">Shows</h1>
+        {queryLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {queryData?.shows.map((show: any) => (
+              <li key={show.title}>{show.title}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </main>
   );
 }
