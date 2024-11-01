02af434cdbc86db7442512544f3acc5e737ae74c
zlac370 <zlac370@live.rhul.ac.uk>
2024-11-01 17:13:39 +0000

    feat: created two new pages, one for the user account and one for the shop

--- product/frontend/src/app/page.tsx
+++ product/frontend/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
   });
 
   return (
-    <main className="flex min-h-screen items-center justify-center text-black">
+    <main className="flex min-h-screen items-center justify-center bg-zinc-100 text-black dark:bg-zinc-900 dark:text-white">
       <div className="flex flex-col">
         <button
           onClick={() => {
