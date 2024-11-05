d7c549f00cc23be06bbceb782e551e7759496b59
zlac370 <zlac370@live.rhul.ac.uk>
2024-11-05 18:56:24 +0000

    fix: removed old login button on landing page

--- product/frontend/src/app/page.tsx
+++ product/frontend/src/app/page.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import { signIn } from "next-auth/react";
-
 export default function HomePage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-zinc-100 text-black dark:bg-zinc-900 dark:text-white">
-      <div className="flex flex-col">
-        <button
-          onClick={() => {
-            signIn();
-          }}
-        >
-          Login
-        </button>
-      </div>
+      <div className="flex flex-col"></div>
     </main>
   );
 }
