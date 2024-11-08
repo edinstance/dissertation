d80116bdd9d7e36afc0c16a8f3febaa11c29fc1a
zlac370 <zlac370@live.rhul.ac.uk>
2024-11-08 02:16:06 +0000

    feat: updated the user service so that duplicate users cant be added

--- product/backend/application/src/main/java/com/finalproject/backend/services/UserService.java
+++ product/backend/application/src/main/java/com/finalproject/backend/services/UserService.java
@@ -5,6 +5,8 @@ import com.finalproject.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 /**
  * Service class for managing User entities.
  */
@@ -28,8 +30,13 @@ public class UserService {
      *
      * @param newUser the user to be created.
      * @return the created user.
+     * @throws IllegalArgumentException an error if a user with the uuid already exists.
      */
     public UserEntity createUser(final UserEntity newUser) {
+        Optional<UserEntity> existingUser = userRepository.findById(newUser.getUserId());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("User with UUID " + newUser.getUserId() + " already exists.");
+        }
         return userRepository.save(newUser);
     }
 }
