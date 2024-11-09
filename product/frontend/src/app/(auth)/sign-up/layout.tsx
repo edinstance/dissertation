1a850d09a8ab2d39a10f3166e9459d4e7147c46d
zlac370 <zlac370@live.rhul.ac.uk>
2024-11-09 23:43:42 +0000

    feat!: updated signup page to create user on the backend after cognito setup. allowed the apollo client to use an api key for authorisation

---
+++ product/frontend/src/app/(auth)/sign-up/layout.tsx
@@ -0,0 +1,26 @@
+import "@/styles/globals.css";
+
+import { auth } from "@/server/auth";
+import { ApolloWrapper } from "@/components/apollo";
+
+const BACKEND_GRAPHQL_ENDPOINT = process.env.BACKEND_GRAPHQL_ENDPOINT ?? "";
+const BACKEND_API_KEY = process.env.API_KEY ?? "";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth();
+
+  return (
+    <html lang="en">
+      <body className={`bg-zinc-100 dark:bg-zinc-900`}>
+        <ApolloWrapper
+          link={BACKEND_GRAPHQL_ENDPOINT}
+          apiKey={BACKEND_API_KEY}
+        >
+          {children}
+        </ApolloWrapper>
+      </body>
+    </html>
+  );
+}
