548f1df4a2693754ac7914c689202feb060d9e92
zlac370 <zlac370@live.rhul.ac.uk>
2024-11-16 12:54:42 +0000

    feat: service checkstyle changes

--- product/backend/application/backendService/src/main/java/com/finalproject/backend/services/UserService.java
+++ product/backend/application/backendService/src/main/java/com/finalproject/backend/services/UserService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.UUID;
 
 /**
  * Service class for managing User entities.
@@ -14,16 +13,19 @@ import java.util.UUID;
 @Service
 public class UserService {
 
+    /**
+     * Repository for accessing user data.
+     */
     private final UserRepository userRepository;
 
     /**
      * Constructs a UserService with the specified UserRepository.
      *
-     * @param userRepository The repository for accessing User entities.
+     * @param inputUserRepository The repository for accessing User entities.
      */
     @Autowired
-    public UserService(final UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(final UserRepository inputUserRepository) {
+        this.userRepository = inputUserRepository;
     }
 
     /**
@@ -31,12 +33,15 @@ public class UserService {
      *
      * @param newUser the user to be created.
      * @return the created user.
-     * @throws IllegalArgumentException an error if a user with the uuid already exists.
+     * @throws IllegalArgumentException error if duplicated UUId.
      */
     public UserEntity createUser(final UserEntity newUser) {
-        Optional<UserEntity> existingUser = userRepository.findById(newUser.getId());
+        Optional<UserEntity> existingUser =
+                userRepository.findById(newUser.getId());
+
         if (existingUser.isPresent()) {
-            throw new IllegalArgumentException("User with UUID " + newUser.getId() + " already exists.");
+            throw new IllegalArgumentException("User with UUID "
+                    + newUser.getId() + " already exists.");
         }
         return userRepository.save(newUser);
     }
---
+++ product/backend/application/backendService/src/main/java/com/finalproject/backend/services/package-info.java
@@ -0,0 +1,5 @@
+
+/**
+ * This package contains all the services.
+ */
+package com.finalproject.backend.services;
