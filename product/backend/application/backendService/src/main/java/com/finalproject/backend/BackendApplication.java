0793926b8f8f0238711028e29ac9fb9065ae2bd3
zlac370 <zlac370@live.rhul.ac.uk>
2024-11-16 12:43:13 +0000

    feat: backend application checkstyle fixes and main package info

--- product/backend/application/backendService/src/main/java/com/finalproject/backend/BackendApplication.java
+++ product/backend/application/backendService/src/main/java/com/finalproject/backend/BackendApplication.java
@@ -3,10 +3,18 @@ package com.finalproject.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for the backend application.
+ */
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
+    /**
+     * Main method to run the Spring Boot application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
---
+++ product/backend/application/backendService/src/main/java/com/finalproject/backend/package-info.java
@@ -0,0 +1,5 @@
+
+/**
+ * This package is the root package and contains the backend code.
+ */
+package com.finalproject.backend;
