c9543469accc1678eb8c3fb1dc7efc2dc337fc54
zlac370 <zlac370@live.rhul.ac.uk>
2024-11-21 23:48:28 +0000

    feat: craeted a user details table

---
+++ product/backend/database/src/sql/tables/user_details.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS user_details (
+    user_id UUID PRIMARY KEY,
+    contact_number VARCHAR(20),
+    address_street VARCHAR(255),
+    address_city VARCHAR(100),
+    address_county VARCHAR(100),
+    address_post_code VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
