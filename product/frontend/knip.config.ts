c1429931e34f9a1bd6769f9fdc90ed8ffb136566
zlac370 <zlac370@live.rhul.ac.uk>
2024-12-06 11:58:32 +0000

    fix: knip ignored next config as it is still in beta and causing issues

--- product/frontend/knip.config.ts
+++ product/frontend/knip.config.ts
@@ -2,6 +2,8 @@ import type { KnipConfig } from "knip";
 
 const config = {
   project: ["src/**/*!", "!src/gql/*", "!cypress/**/*"],
+  ignore: ["src/server/auth/index.ts"],
+  //   Ignore Next auth config as it is still in beta so there are some issues.
 } satisfies KnipConfig;
 
 export default config;
