c0922a7429705def1e73a492188295b7a27ac57e
zlac370 <zlac370@live.rhul.ac.uk>
2024-12-06 00:38:03 +0000

    feat: created component tests for button, header and navigation

--- product/frontend/cypress/component/components/Header.cy.tsx
+++ product/frontend/cypress/component/components/Header.cy.tsx
@@ -1,7 +1,5 @@
-
 import Header from "@/components/Header";
 import SessionProvider from "@/components/Providers/Auth";
-import Divivder from "@/components/ui/Divider";
 
 describe("<Header />", () => {
   it("renders", () => {
