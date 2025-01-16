7a1fa047802fc22f1e383d0dbd7dcbbdcddcfe07
zlac370 <zlac370@live.rhul.ac.uk>
2025-01-16 14:58:15 +0000

    feat: added delete user option to the account settings

---
+++ product/frontend/src/actions/delete-user.tsx
@@ -0,0 +1,51 @@
+"use server";
+
+import cognitoClient from "@/lib/cognito";
+import stripe from "@/lib/stripe";
+import { auth } from "@/server/auth";
+import { findCustomerByUserId } from "@/utils/stripe";
+import { AdminDeleteUserCommand } from "@aws-sdk/client-cognito-identity-provider";
+
+async function deleteUser() {
+  try {
+    const session = await auth();
+
+    if (!session) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = session.user.id;
+
+    // Find the Stripe customer associated with the user
+    const stripeCustomer = await findCustomerByUserId(userId);
+    if (!stripeCustomer) {
+      throw new Error("Stripe customer not found");
+    }
+
+    // Delete the Stripe customer
+    await stripe.customers.del(stripeCustomer.id);
+
+    // Ensure the Cognito User Pool ID is set
+    const userPoolId = process.env.COGNITO_USER_POOL_ID;
+    if (!userPoolId) {
+      throw new Error("Cognito User Pool ID is not defined");
+    }
+
+    // Delete the user from Cognito
+    const deleteUserCommand = new AdminDeleteUserCommand({
+      UserPoolId: userPoolId,
+      Username: session.user.email,
+    });
+
+    await cognitoClient.send(deleteUserCommand);
+
+    return { success: true, message: `User ${userId} deleted successfully.` };
+  } catch (error) {
+    console.error("Error in deleteUser:", error);
+    return {
+      error: error instanceof Error ? error.message : "Error deleting user",
+    };
+  }
+}
+
+export default deleteUser;
--- product/frontend/src/app/(application)/account/page.tsx
+++ product/frontend/src/app/(application)/account/page.tsx
@@ -1,3 +1,46 @@
+"use client";
+import deleteUser from "@/actions/delete-user";
+import { Button } from "@/components/ui/Button";
+import Modal from "@/components/ui/Modal";
+import { DialogTitle } from "@headlessui/react";
+import { redirect } from "next/navigation";
+import { useState } from "react";
+
 export default function Account() {
-  return <></>;
+  const handleDeleteUser = async () => {
+    await deleteUser();
+    redirect("/");
+  };
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="pt-8">
+      <Button
+        onClick={() => {
+          setOpen(true);
+        }}
+        color="destructive"
+      >
+        Delete User
+      </Button>
+      <Modal open={open} setOpen={setOpen}>
+        <DialogTitle className="text-lg text-black dark:text-white">
+          Are you sure?
+        </DialogTitle>
+        <p className="text-md text-black dark:text-white">
+          This action cannot be undone. Are you sure you want to delete your
+          account?
+        </p>
+        <div className="flex justify-end space-x-4">
+          <Button onClick={() => setOpen(false)} color="blue">
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteUser} color="destructive">
+            Delete
+          </Button>
+        </div>
+      </Modal>
+    </div>
+  );
 }
