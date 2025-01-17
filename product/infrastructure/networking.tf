a8bd1ad67b9429d3b0a0b0c2fb87e9c1994b1354
zlac370 <zlac370@live.rhul.ac.uk>
2025-01-17 19:19:25 +0000

    docs: added misconfig todo

--- product/infrastructure/networking.tf
+++ product/infrastructure/networking.tf
@@ -1,4 +1,8 @@
 
+# TODO: Consider adding VPC Flow Logs (MEDIUM Security Risk)
+# https://avd.aquasec.com/misconfig/avd-aws-0178
+
+
 # This creates a VPC with CIDR block
 resource "aws_vpc" "vpc" {
     cidr_block = var.vpc_cidr
