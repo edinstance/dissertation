935a8756ad430ee96249980e73eddeb2eed3d52a
zlac370 <zlac370@live.rhul.ac.uk>
2025-01-23 14:46:59 +0000

    feat: added iam policy so ecs tasks can get ssm params

--- product/infrastructure/modules/iam/ecs.tf
+++ product/infrastructure/modules/iam/ecs.tf
@@ -17,3 +17,31 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   ]
 })
 }
+
+# IAM policy for ssm access
+resource "aws_iam_policy" "ssm_access_policy" {
+  name        = "ecs_ssm_access_policy"
+  description = "Policy to allow ECS tasks to access SSM parameters"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:GetParametersByPath"
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+
+# Attach the SSM Access Policy to the ECS Task Execution Role
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
+  policy_arn = aws_iam_policy.ssm_access_policy.arn
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
