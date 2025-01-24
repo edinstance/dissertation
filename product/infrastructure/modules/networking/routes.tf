10195575402a6b19c3e1a084a64074b12ac71bb2
zlac370 <zlac370@live.rhul.ac.uk>
2025-01-24 02:42:07 +0000

    refactor: combined public and private routes and route tables

--- product/infrastructure/modules/networking/routes.tf
+++ product/infrastructure/modules/networking/routes.tf
@@ -17,3 +17,19 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+# This creates a private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.environment}-private-rt"
+  }
+}
+
+# Associate private subnets with the private route table
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
