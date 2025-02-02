cf3dcf1f2b0a7306c3460264512e614e238072d5
zlac370 <zlac370@live.rhul.ac.uk>
2025-02-02 16:37:04 +0000

    fix: updated the button component

--- product/frontend/src/components/ui/Button.tsx
+++ product/frontend/src/components/ui/Button.tsx
@@ -18,21 +18,23 @@ type ButtonProps =
 const styles = {
   base: [
     "flex items-center justify-center rounded-lg border border-transparent px-4 py-2 text-sm " +
-      "focus:outline-none focus:ring-2 focus:ring-offset-2 w-full" +
+      "w-full" +
       "data-[disabled]:bg-red-500 data-[disabled]:opacity-50",
   ],
   variants: {
-    solid: "border border-transparent",
+    solid: "border border-transparent outline-none focus:outline-none",
     outline: "border outline",
   },
   colors: {
     blue: {
-      solid: "bg-blue-600 text-white shadow-sm hover:bg-blue-500 focus:ring-blue-500",
+      solid:
+        "bg-blue-600 text-white shadow-sm hover:bg-blue-500 focus:ring-blue-500",
       outline:
         "border-blue-600 text-blue-600 hover:bg-blue-50 dark:hover:bg-blue-950 focus:ring-blue-500",
     },
     destructive: {
-      solid: "bg-red-600 text-white shadow-sm hover:bg-red-500 focus:ring-red-500",
+      solid:
+        "bg-red-600 text-white shadow-sm hover:bg-red-500 focus:ring-red-500",
       outline: "border-red-600 text-red-600 hover:bg-red-50 focus:ring-red-500",
     },
   },
@@ -48,7 +50,7 @@ export function Button({
     className,
     styles.base,
     styles.variants[variant],
-    styles.colors[color][variant]
+    styles.colors[color][variant],
   );
 
   return typeof props.href === "undefined" ? (
