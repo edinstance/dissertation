c0dc5465e2a9d6fde986e478c0087fd48938d8b9
zlac370 <zlac370@live.rhul.ac.uk>
2025-02-03 22:18:09 +0000

    fix: formatted code

--- product/frontend/src/stores/SearchStore.tsx
+++ product/frontend/src/stores/SearchStore.tsx
@@ -2,15 +2,15 @@ import { SearchItemsQuery } from "@/gql/graphql";
 import { create } from "zustand";
 
 interface SearchStore {
-    searchQuery: string;
-    results: SearchItemsQuery["searchForItems"];
-    setSearchQuery: (query: string) => void;
-    setSearchResults: (results: SearchItemsQuery["searchForItems"]) => void;
-  }
-  
-  export const useSearchStore = create<SearchStore>((set) => ({
-    searchQuery: "",
-    results: [],
-    setSearchQuery: (searchQuery) => set({ searchQuery }),
-    setSearchResults: (results) => set({ results }),
-  }));
+  searchQuery: string;
+  results: SearchItemsQuery["searchForItems"];
+  setSearchQuery: (query: string) => void;
+  setSearchResults: (results: SearchItemsQuery["searchForItems"]) => void;
+}
+
+export const useSearchStore = create<SearchStore>((set) => ({
+  searchQuery: "",
+  results: [],
+  setSearchQuery: (searchQuery) => set({ searchQuery }),
+  setSearchResults: (results) => set({ results }),
+}));
