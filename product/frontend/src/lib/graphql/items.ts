553ad25511d9c90f37d9951933081c42c0cfbd7a
edinstance <edinstance@gmail.com>
2025-02-21 16:51:44 +0000

    feat: created get item by id query on frontend and updated schema using codegen

--- product/frontend/src/lib/graphql/items.ts
+++ product/frontend/src/lib/graphql/items.ts
@@ -21,6 +21,27 @@ export const SEARCH_FOR_ITEMS = graphql(`
   }
 `);
 
+export const GET_ITEM_BY_ID_QUERY = graphql(`
+  query GetItemById($id: String!) {
+    getItemById(id: $id) {
+      id
+      name
+      description
+      isActive
+      endingTime
+      price
+      stock
+      category
+      images
+      seller {
+        id
+        name
+        email
+      }
+    }
+  }
+`);
+
 export const SAVE_ITEM_MUTATION = graphql(`
   mutation SaveItem($itemInput: ItemInput!) {
     saveItem(itemInput: $itemInput) {
