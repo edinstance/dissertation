10c4b23bb34ea06b8157f3b75329d2f58c6d2775
edinstance <edinstance@gmail.com>
2025-02-21 16:23:49 +0000

    feat: created query for getting an item by its id

--- product/backend/application/backendService/src/test/java/com/finalproject/backend/QueryTests/ItemQueryTests.java
+++ product/backend/application/backendService/src/test/java/com/finalproject/backend/QueryTests/ItemQueryTests.java
@@ -4,6 +4,7 @@ import com.finalproject.backend.entities.ItemEntity;
 import com.finalproject.backend.entities.UserEntity;
 import com.finalproject.backend.queries.ItemQueries;
 import com.finalproject.backend.services.ItemService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -25,23 +26,37 @@ public class ItemQueryTests {
 
   @InjectMocks
   ItemQueries itemQueries;
+  private ItemEntity item;
 
-  @Test
-  public void testSearchForItem() {
-    UserEntity userEntity = new UserEntity(UUID.randomUUID(), "seller@test.com",
+  @BeforeEach
+  void setUp() {
+    UserEntity user = new UserEntity(UUID.randomUUID(), "seller@test.com",
             "Seller Name");
 
     UUID id = UUID.randomUUID();
     String endingTime = new Date().toString();
-    ItemEntity item = new ItemEntity(id, "name",
+    item = new ItemEntity(id, "name",
             "description", endingTime, new BigDecimal("2.2"),
-            1, "category", List.of("image"), userEntity);
+            1, "category", List.of("image"), user);
+  }
 
+  @Test
+  public void testSearchForItem() {
     when(itemService.searchForItemsByName("name")).thenReturn(List.of(item));
 
     List<ItemEntity> result = itemQueries.searchForItems("name");
 
     assert result.size() == 1;
     assert result.contains(item);
+  }
+
+  @Test
+  public void testGetItemById() {
+
+    when(itemService.getItemById(item.getId())).thenReturn(item);
+
+    ItemEntity result = itemQueries.getItemById(item.getId().toString());
+
+    assert result.getId().equals(item.getId());
   }
 }
