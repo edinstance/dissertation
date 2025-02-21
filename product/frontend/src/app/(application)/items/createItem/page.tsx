4f4ca9d486a825dbc039d76cd747061d7c75ee7e
edinstance <edinstance@gmail.com>
2025-02-21 10:32:51 +0000

    feat: created page for creating items

---
+++ product/frontend/src/app/(application)/items/createItem/page.tsx
@@ -0,0 +1,261 @@
+"use client";
+
+import { deleteS3Object, getPresignedUrls } from "@/actions/s3";
+import { Button } from "@/components/ui/Button";
+import Divider from "@/components/ui/Divider";
+import ImageUpload from "@/components/ui/ImageUpload";
+import { Input } from "@/components/ui/Input";
+import { Select } from "@/components/ui/Select";
+import { TextArea } from "@/components/ui/TextArea";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
+type FormData = {
+  name: string;
+  description: string;
+  duration: string;
+  price: number;
+  stock: number;
+  category: string;
+  images: string[];
+};
+
+const durationOptions = [
+  { value: "1_day", label: "1 Day" },
+  { value: "3_days", label: "3 Days" },
+  { value: "7_days", label: "7 Days" },
+];
+
+export default function CreateItem() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    control,
+    setValue,
+  } = useForm<FormData>();
+
+  async function onSubmit(data: FormData) {
+  
+  }
+
+  const [uploadedImages, setUploadedImages] = useState<UploadedImage[]>([]);
+
+  const handleImagesChange = async (files: File[]) => {
+    const result = await getPresignedUrls(
+      files.map((file) => ({ name: file.name, type: file.type })),
+    );
+
+    if (!result.success || !result.urls) {
+      throw new Error("Failed to get presigned URLs");
+    }
+
+    // Upload files to S3
+    const uploadedUrls = await Promise.all(
+      files.map(async (file, index) => {
+        const urlData = result.urls[index];
+        if (!urlData) throw new Error(`No URL data for file ${file.name}`);
+
+        const { presignedUrl, publicUrl, key } = urlData;
+
+        const response = await fetch(presignedUrl, {
+          method: "PUT",
+          body: file,
+          headers: {
+            "Content-Type": file.type,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to upload ${file.name}`);
+        }
+
+        return {
+          file,
+          publicUrl,
+          key,
+        };
+      }),
+    );
+
+
+    setUploadedImages((prev) => [...prev, ...uploadedUrls]);
+    
+    const allUrls = [...uploadedImages, ...uploadedUrls].map(
+      (img) => img.publicUrl
+    );
+    setValue("images", allUrls);
+  };
+
+  function removeFile(key: string) {
+    setUploadedImages((prev) => prev.filter((img) => img.key !== key));
+    deleteS3Object(key);
+  }
+
+  return (
+    <div className="pt-20">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="min-h-screen max-w-3xl py-20 pl-8"
+      >
+        <h1 className="pb-4 text-4xl">List an Item</h1>
+        <Divider />
+        <div className="grid gap-x-8 gap-y-6 py-4 sm:grid-cols-2">
+          <div className="space-y-4">
+            <h2>Basic Details</h2>
+            <p className="text-xs text-gray-500">
+              Enter the basic information about your item.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <div className="min-h-12 space-y-1">
+              <Input
+                type="text"
+                placeholder="Item Name"
+                {...register("name", { required: "Name is required" })}
+                invalid={errors?.name ? true : false}
+              />
+              {errors.name && (
+                <p className="text-xs text-red-500">{errors.name.message}</p>
+              )}
+            </div>
+
+            <div className="min-h-32 space-y-1">
+              <TextArea
+                placeholder="Description"
+                rows={4}
+                {...register("description", {
+                  required: "Description is required",
+                })}
+                invalid={errors?.description ? true : false}
+              />
+              {errors.description && (
+                <p className="text-xs text-red-500">
+                  {errors.description.message}
+                </p>
+              )}
+            </div>
+            <div className="min-h-12 space-y-1">
+              <Input
+                type="text"
+                placeholder="Category"
+                {...register("category", { required: "Category is required" })}
+                invalid={errors?.category ? true : false}
+              />
+              {errors.category && (
+                <p className="text-xs text-red-500">
+                  {errors.category.message}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <Divider />
+        <div className="grid gap-x-8 gap-y-6 py-4 sm:grid-cols-2">
+          <div className="space-y-4">
+            <h2>Pricing and Stock</h2>
+            <p className="text-xs text-gray-500">
+              Set the price and available stock for your item.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <div className="min-h-12 space-y-1">
+              <Input
+                type="number"
+                step="0.01"
+                placeholder="Price"
+                {...register("price", { required: "Price is required" })}
+                invalid={errors?.price ? true : false}
+              />
+              {errors.price && (
+                <p className="text-xs text-red-500">{errors.price.message}</p>
+              )}
+            </div>
+            <div className="min-h-12 space-y-1">
+              <Input
+                type="number"
+                placeholder="Stock"
+                {...register("stock", { required: "Stock is required" })}
+                invalid={errors?.stock ? true : false}
+              />
+              {errors.stock && (
+                <p className="text-xs text-red-500">{errors.stock.message}</p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <Divider />
+        <div className="grid gap-x-8 gap-y-6 py-4 sm:grid-cols-2">
+          <div className="space-y-4">
+            <h2>Images</h2>
+            <p className="text-xs text-gray-500">
+              Upload images of your item. You can select multiple images.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <div className="min-h-12 space-y-1">
+              <ImageUpload
+                onImagesChange={handleImagesChange}
+                removeFile={removeFile}
+                value={uploadedImages}
+                maxFiles={5}
+                maxSize={5 * 1024 * 1024}
+              />
+              {errors.images && (
+                <p className="text-xs text-red-500">{errors.images.message}</p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <Divider />
+
+        <div className="grid gap-x-8 gap-y-6 py-4 sm:grid-cols-2">
+          <div className="space-y-4">
+            <h2>Duration</h2>
+            <p className="text-xs text-gray-500">
+              Set how long you want this item to be available for.
+            </p>
+          </div>
+          <div className="space-y-4">
+            <div className="min-h-12 space-y-1">
+              <Controller
+                name="duration"
+                control={control}
+                rules={{ required: "Duration is required" }}
+                render={({ field }) => (
+                  <Select
+                    options={durationOptions}
+                    value={
+                      durationOptions.find(
+                        (option) => option.value === field.value,
+                      ) || null
+                    }
+                    onChange={(selected) => field.onChange(selected.value)}
+                    placeholder="Select Duration"
+                    invalid={!!errors.duration}
+                  />
+                )}
+              />
+              {errors.duration && (
+                <p className="text-xs text-red-500">
+                  {errors.duration.message}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <Divider />
+        <div className="flex flex-row items-end justify-end space-x-4 pt-4">
+          <Button type="submit" className="max-w-32">
+            Create Item
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
