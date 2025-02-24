e5439964024c398c7ee27e72ab58b6aeff1217ec
edinstance <edinstance@gmail.com>
2025-02-24 22:39:13 +0000

    feat: created search items response dto

---
+++ product/backend/application/backendService/src/test/java/com/finalproject/backend/dtoTests/SearchedItemsResponseTests.java
@@ -0,0 +1,52 @@
+package com.finalproject.backend.dtoTests;
+
+import com.finalproject.backend.dto.SearchedItemsResponse;
+import com.finalproject.backend.entities.ItemEntity;
+import com.finalproject.backend.helpers.Pagination;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SearchedItemsResponseTests{
+
+  private SearchedItemsResponse searchedItemsResponse;
+  private final ItemEntity item = new ItemEntity();
+  private final Pagination pagination = new Pagination();
+
+  @Test
+  public void searchedItemsResponseDefaultConstructorTest(){
+    searchedItemsResponse = new SearchedItemsResponse();
+
+    assertNotNull(searchedItemsResponse);
+  }
+
+  @Test
+  public void searchedItemsResponseConstructorTest(){
+    searchedItemsResponse = new SearchedItemsResponse(List.of(item), pagination);
+
+    assertNotNull(searchedItemsResponse);
+  }
+
+  @BeforeEach
+  public void setUp(){
+    searchedItemsResponse = new SearchedItemsResponse(List.of(item), pagination);
+  }
+
+  @Test
+  public void itemsMethodsTest(){
+    List<ItemEntity> items = List.of(item);
+    searchedItemsResponse.setItems(items);
+
+    assert searchedItemsResponse.getItems().getFirst().equals(item);
+  }
+
+  @Test
+  public void paginationMethodsTest(){
+    searchedItemsResponse.setPagination(pagination);
+
+    assert searchedItemsResponse.getPagination().equals(pagination);
+  }
+}
