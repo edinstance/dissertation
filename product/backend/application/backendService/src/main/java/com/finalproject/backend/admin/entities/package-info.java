9a14dee418b571017d5aa792f7350a2fb2c25de8
edinstance <edinstance@gmail.com>
2025-03-22 23:22:37 +0000

    feat: created an admin entity and tests

---
+++ product/backend/application/backendService/src/main/java/com/finalproject/backend/admin/entities/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the admin entities.
+ */
+package com.finalproject.backend.admin.entities;
---
+++ product/backend/application/backendService/src/test/java/com/finalproject/backend/AdminTests/EntityTests/AdminEntityTests.java
@@ -0,0 +1,77 @@
+package com.finalproject.backend.AdminTests.EntityTests;
+
+import com.finalproject.backend.admin.entities.AdminEntity;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AdminEntityTests {
+
+  private static AdminEntity adminEntity;
+
+  @Test
+  public void testDefaultConstructor() {
+    AdminEntity adminEntity = new AdminEntity();
+    assertNotNull(adminEntity);
+    assertFalse(adminEntity.isSuperAdmin());
+    assertFalse(adminEntity.getIsDeleted());
+  }
+
+  @Test
+  public void testConstructor() {
+    AdminEntity adminEntity = new AdminEntity(UUID.randomUUID(),
+            false, "ACTIVE", UUID.randomUUID(), UUID.randomUUID());
+    assertNotNull(adminEntity);
+    assertFalse(adminEntity.isSuperAdmin());
+    assertEquals("ACTIVE", adminEntity.getStatus());
+  }
+
+  @BeforeAll
+  public static void setUp() {
+    adminEntity = new AdminEntity(UUID.randomUUID(),
+            false, "ACTIVE", UUID.randomUUID(), UUID.randomUUID());
+  }
+
+  @Test
+  public void testIdMethods() {
+    UUID newId = UUID.randomUUID();
+    adminEntity.setUserId(newId);
+    assertEquals(newId, adminEntity.getUserId());
+  }
+
+  @Test
+  public void testSuperAdminMethods() {
+    adminEntity.setSuperAdmin(true);
+    assertTrue(adminEntity.isSuperAdmin());
+  }
+
+  @Test
+  public void testStatusMethods() {
+    adminEntity.setStatus("DEACTIVATED");
+    assertEquals("DEACTIVATED", adminEntity.getStatus());
+  }
+
+  @Test
+  public void testCreatedByMethods() {
+    UUID newId = UUID.randomUUID();
+    adminEntity.setCreatedBy(newId);
+    assertEquals(newId, adminEntity.getCreatedBy());
+  }
+
+  @Test
+  public void testLastUpdatedByMethods() {
+    UUID newId = UUID.randomUUID();
+    adminEntity.setLastUpdatedBy(newId);
+    assertEquals(newId, adminEntity.getLastUpdatedBy());
+  }
+
+  @Test
+  public void testIsDeletedMethods() {
+    adminEntity.setIsDeleted(true);
+    assertTrue(adminEntity.getIsDeleted());
+  }
+
+}
