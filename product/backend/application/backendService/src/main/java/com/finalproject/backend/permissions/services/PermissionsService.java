9d483e24360de12beda5866f2e08936961e699dc
edinstance <edinstance@gmail.com>
2025-03-24 22:11:38 +0000

    feat: added authorization check to get all admin permissions

--- product/backend/application/backendService/src/main/java/com/finalproject/backend/permissions/services/PermissionsService.java
+++ product/backend/application/backendService/src/main/java/com/finalproject/backend/permissions/services/PermissionsService.java
@@ -1,9 +1,13 @@
 package com.finalproject.backend.permissions.services;
 
+import com.finalproject.backend.common.exceptions.UnauthorisedException;
 import com.finalproject.backend.common.helpers.AuthHelpers;
+import com.finalproject.backend.permissions.authorizers.AdminAuthorizer;
 import com.finalproject.backend.permissions.entities.PermissionView;
 import com.finalproject.backend.permissions.repositories.PermissionViewRepository;
+import com.finalproject.backend.permissions.types.Actions;
 import com.finalproject.backend.permissions.types.AdminViewTypes;
+import com.finalproject.backend.permissions.types.Resources;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,18 +24,29 @@ public class PermissionsService {
    */
   private final PermissionViewRepository permissionsViewRepository;
 
+  /**
+   * The helper for authentication.
+   */
   private final AuthHelpers authHelpers;
+
+  /**
+   * The authorizer for admin permissions.
+   */
+  private final AdminAuthorizer adminAuthorizer;
 
   /**
    * Constructs a PermissionsService with the specified PermissionViewRepository.
    *
    * @param permissionsViewRepository The repository for accessing views of the permissions.
+   * @param authHelpers               The helper for authentication.
+   * @param adminAuthorizer           The authorizer for admin permissions.
    */
   @Autowired
   public PermissionsService(PermissionViewRepository permissionsViewRepository,
-                            AuthHelpers authHelpers) {
+                            AuthHelpers authHelpers, AdminAuthorizer adminAuthorizer) {
     this.permissionsViewRepository = permissionsViewRepository;
     this.authHelpers = authHelpers;
+    this.adminAuthorizer = adminAuthorizer;
   }
 
   /**
@@ -40,6 +55,19 @@ public class PermissionsService {
    * @return all the admin permissions.
    */
   public List<PermissionView> getAllAdminPermissions() {
+
+    boolean authorized = adminAuthorizer.authorize(
+            authHelpers.getCurrentUserId(),
+            Resources.ADMIN_PERMISSIONS,
+            Actions.READ,
+            AdminViewTypes.ALL
+
+    );
+
+    if (!authorized) {
+      throw new UnauthorisedException("Admin is not authorized to view all admin permissions");
+    }
+
     return permissionsViewRepository.getAllAdminPermissions(AdminViewTypes.ALL.getViewTypeName());
   }
 
--- product/backend/application/backendService/src/test/java/com/finalproject/backend/PermissionTests/ServiceTests/PermissionServiceTests.java
+++ product/backend/application/backendService/src/test/java/com/finalproject/backend/PermissionTests/ServiceTests/PermissionServiceTests.java
@@ -1,10 +1,14 @@
 package com.finalproject.backend.PermissionTests.ServiceTests;
 
+import com.finalproject.backend.common.exceptions.UnauthorisedException;
 import com.finalproject.backend.common.helpers.AuthHelpers;
+import com.finalproject.backend.permissions.authorizers.AdminAuthorizer;
 import com.finalproject.backend.permissions.entities.PermissionView;
 import com.finalproject.backend.permissions.repositories.PermissionViewRepository;
 import com.finalproject.backend.permissions.services.PermissionsService;
+import com.finalproject.backend.permissions.types.Actions;
 import com.finalproject.backend.permissions.types.AdminViewTypes;
+import com.finalproject.backend.permissions.types.Resources;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -14,7 +18,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +33,9 @@ public class PermissionServiceTests {
   @Mock
   private PermissionViewRepository permissionViewRepository;
 
+  @Mock
+  private AdminAuthorizer adminAuthorizer;
+
   @InjectMocks
   private PermissionsService permissionsService;
 
@@ -34,6 +44,8 @@ public class PermissionServiceTests {
   @Test
   public void testGetAllAdminPermissionsEmpty() {
     when(permissionViewRepository.getAllAdminPermissions(AdminViewTypes.ALL.getViewTypeName())).thenReturn(List.of());
+    when(authHelpers.getCurrentUserId()).thenReturn(adminId);
+    when(adminAuthorizer.authorize(eq(adminId), eq(Resources.ADMIN_PERMISSIONS), eq(Actions.READ), eq(AdminViewTypes.ALL))).thenReturn(true);
 
     List<PermissionView> allAdminPermissions = permissionsService.getAllAdminPermissions();
 
@@ -43,10 +55,22 @@ public class PermissionServiceTests {
   @Test
   public void testGetAllAdminPermissions() {
     when(permissionViewRepository.getAllAdminPermissions(AdminViewTypes.ALL.getViewTypeName())).thenReturn(List.of(new PermissionView()));
+    when(authHelpers.getCurrentUserId()).thenReturn(adminId);
+    when(adminAuthorizer.authorize(eq(adminId), eq(Resources.ADMIN_PERMISSIONS), eq(Actions.READ), eq(AdminViewTypes.ALL))).thenReturn(true);
 
     List<PermissionView> allAdminPermissions = permissionsService.getAllAdminPermissions();
 
     assertFalse(allAdminPermissions.isEmpty());
+  }
+
+  @Test
+  public void testGetAllAdminPermissionsNotAuthorized() {
+    when(authHelpers.getCurrentUserId()).thenReturn(adminId);
+    when(adminAuthorizer.authorize(eq(adminId), eq(Resources.ADMIN_PERMISSIONS), eq(Actions.READ), eq(AdminViewTypes.ALL))).thenReturn(false);
+
+    UnauthorisedException exception = assertThrows(UnauthorisedException.class, () -> permissionsService.getAllAdminPermissions());
+
+    assertTrue(exception.getMessage().contains("Admin is not authorized to view all admin permissions"));
   }
 
   @Test
@@ -62,7 +86,7 @@ public class PermissionServiceTests {
   @Test
   public void testGetAdminPermissionsById() {
     when(authHelpers.getCurrentUserId()).thenReturn(adminId);
-    when(permissionViewRepository.getAdminPermissionsById(adminId,AdminViewTypes.ALL.getViewTypeName())).thenReturn(List.of(new PermissionView()));
+    when(permissionViewRepository.getAdminPermissionsById(adminId, AdminViewTypes.ALL.getViewTypeName())).thenReturn(List.of(new PermissionView()));
 
     List<PermissionView> allAdminPermissions = permissionsService.getCurrentAdminPermissions();
 
