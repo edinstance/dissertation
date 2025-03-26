0636cf82ac1281ea55816f02647c08651234d2bc
edinstance <edinstance@gmail.com>
2025-03-26 01:13:41 +0000

    fix: upadted dropdown

--- product/frontend/src/components/ui/DropDown.tsx
+++ product/frontend/src/components/ui/DropDown.tsx
@@ -1,5 +1,5 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 export interface DropdownOption {
   label: string;
@@ -7,17 +7,22 @@ export interface DropdownOption {
   className?: string;
 }
 
-export default function DropDown({
+export function DropDown({
   title,
   options,
+  disabled,
 }: {
   title: string;
   options: DropdownOption[];
+  disabled?: boolean;
 }) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-black shadow-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-black dark:text-white dark:ring-gray-700 dark:hover:bg-gray-400">
+        <MenuButton
+          disabled={disabled}
+          className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-black shadow-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-black dark:text-white dark:ring-gray-700 dark:hover:bg-gray-400"
+        >
           {title}
           <ChevronDownIcon
             aria-hidden="true"
@@ -30,7 +35,7 @@ export default function DropDown({
         transition
         className="absolute right-0 z-10 mt-2 w-32 min-w-fit origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 dark:bg-black"
       >
-        <div className="py-1">
+        <div className="py-1 flex flex-col">
           {options.map((option) => (
             <MenuItem
               key={option.label}
@@ -46,3 +51,5 @@ export default function DropDown({
     </Menu>
   );
 }
+
+export default DropDown;
