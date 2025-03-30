a6d872644d3a9591a48ac129054b73dd959af0d7
edinstance <edinstance@gmail.com>
2025-03-30 22:01:05 +0100

    fix: fixed component tests

--- product/frontend/cypress/component/components/UserNavigation.cy.tsx
+++ product/frontend/cypress/component/components/UserNavigation.cy.tsx
@@ -1,7 +1,8 @@
-import UserNavigation from "@/components/Users/UserNavigation";
+import TabNavigation from "@/components/ui/TabNavigation";
+import { USER_NAVIGATION_LINKS } from "@/constants/UserNavigation";
 
 describe("<UserNavigation />", () => {
   it("renders", () => {
-    cy.mount(<UserNavigation />);
+    cy.mount(<TabNavigation links={USER_NAVIGATION_LINKS} />);
   });
 });
