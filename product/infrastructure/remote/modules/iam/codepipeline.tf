86cb6d16bfd133c3ffb797002bb1ab9ea8187180
zlac370 <zlac370@live.rhul.ac.uk>
2025-04-08 01:04:58 +0100

    fix: added s3 full access to codepipeline

--- product/infrastructure/remote/modules/iam/codepipeline.tf
+++ product/infrastructure/remote/modules/iam/codepipeline.tf
@@ -26,11 +26,7 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
       {
         Effect = "Allow"
         Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:GetBucketVersioning",
-          "s3:PutObject",
-          "s3:ListBucket"
+          "s3:*",
         ]
         Resource = [
           "*"
