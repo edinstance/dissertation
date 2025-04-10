b9583b686580349a23c37a8023ae37c33ff1bad5
zlac370 <zlac370@live.rhul.ac.uk>
2025-04-10 18:29:48 +0100

    fix: update terraform tables

--- product/infrastructure/shared/dynamodb/chats.tf
+++ product/infrastructure/shared/dynamodb/chats.tf
@@ -1,4 +1,3 @@
-
 resource "aws_dynamodb_table" "chats" {
   name         = "${var.environment}-chats"
   billing_mode = "PROVISIONED"
@@ -6,9 +5,13 @@ resource "aws_dynamodb_table" "chats" {
   read_capacity  = 1
   write_capacity = 1
 
-  hash_key  = "chatId"
+  hash_key  = "conversationId"
   range_key = "createdAt"
 
+  attribute {
+    name = "conversationId"
+    type = "S"
+  }
 
   attribute {
     name = "chatId"
@@ -23,6 +26,17 @@ resource "aws_dynamodb_table" "chats" {
   attribute {
     name = "userId"
     type = "S"
+  }
+
+  global_secondary_index {
+    name      = "chatId-index"
+    hash_key  = "chatId"
+    range_key = "createdAt"
+
+    read_capacity  = 1
+    write_capacity = 1
+
+    projection_type = "ALL"
   }
 
   global_secondary_index {
