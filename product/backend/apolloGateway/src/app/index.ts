e8228a350b246aa2555d4cbe17d908b731753f27
zlac370 <zlac370@live.rhul.ac.uk>
2025-04-13 17:01:27 +0100

    feat: added healthcheck to gateway and fixed test config

--- product/backend/apolloGateway/src/app/index.ts
+++ product/backend/apolloGateway/src/app/index.ts
@@ -3,8 +3,9 @@ import {
   IntrospectAndCompose,
   RemoteGraphQLDataSource,
 } from "@apollo/gateway";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
@@ -45,46 +46,59 @@ const gateway = new ApolloGateway({
   },
 });
 
-const server = new ApolloServer({
-  gateway,
-  context: ({ req }) => {
-    const authHeader = req.headers["authorization"] || null;
-    const apiKey = req.headers["x-api-key"] || null;
-    const forwardedFor = req.headers["x-forwarded-for"] || null;
-    const userAgent = req.headers["user-agent"] || null;
+async function startServer() {
+  const app = express();
 
-    return {
-      authHeader,
-      apiKey,
-      forwardedFor,
-      userAgent,
-    };
-  },
-  cors: {
-    origin: [originUrl, "https://studio.apollographql.com"],
-    credentials: true,
-    allowedHeaders: [
-      "x-api-key",
-      "x-forwarded-for",
-      "user-agent",
-      "Authorization",
-      "Content-Type",
-      "access-control-allow-origin",
-    ],
-    exposedHeaders: [
-      "x-api-key",
-      "x-forwarded-for",
-      "user-agent",
-      "access-control-allow-origin",
-    ],
-  },
-});
-
-server
-  .listen()
-  .then(() => {
-    console.log("Server is running!");
-  })
-  .catch((err) => {
-    console.error("Server failed to start:", err);
+  app.get("/health", (req, res) => {
+    res.status(200).send("OK");
   });
+
+  const server = new ApolloServer({
+    gateway,
+    context: ({ req }) => {
+      const authHeader = req.headers["authorization"] || null;
+      const apiKey = req.headers["x-api-key"] || null;
+      const forwardedFor = req.headers["x-forwarded-for"] || null;
+      const userAgent = req.headers["user-agent"] || null;
+
+      return {
+        authHeader,
+        apiKey,
+        forwardedFor,
+        userAgent,
+      };
+    },
+  });
+
+  await server.start();
+  server.applyMiddleware({
+    app,
+    cors: {
+      origin: [originUrl, "https://studio.apollographql.com"],
+      credentials: true,
+      allowedHeaders: [
+        "x-api-key",
+        "x-forwarded-for",
+        "user-agent",
+        "Authorization",
+        "Content-Type",
+        "access-control-allow-origin",
+      ],
+      exposedHeaders: [
+        "x-api-key",
+        "x-forwarded-for",
+        "user-agent",
+        "access-control-allow-origin",
+      ],
+    },
+  });
+  const PORT = process.env.PORT || 4000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+
+startServer().catch((err) => {
+  console.error("Server failed to start:", err);
+});
