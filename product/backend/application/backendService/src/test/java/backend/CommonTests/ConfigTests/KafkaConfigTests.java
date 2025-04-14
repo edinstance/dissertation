414610d7d276f39b347bf0bf02e4bc4031945e76
zlac370 <zlac370@live.rhul.ac.uk>
2025-04-14 19:22:17 +0100

    fix: formatted tests

--- product/backend/application/backendService/src/test/java/backend/CommonTests/ConfigTests/KafkaConfigTests.java
+++ product/backend/application/backendService/src/test/java/backend/CommonTests/ConfigTests/KafkaConfigTests.java
@@ -17,34 +17,33 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 public class KafkaConfigTests {
 
- @Mock
- private KafkaAdmin kafkaAdmin;
+  @Mock
+  private KafkaAdmin kafkaAdmin;
 
- @InjectMocks
- private KafkaConfig kafkaConfig;
+  @InjectMocks
+  private KafkaConfig kafkaConfig;
 
- @Test
- public void testInitializeKafka_Success() {
-   String[] topics = {"test-topic1", "test-topic2"};
-   ReflectionTestUtils.setField(kafkaConfig, "topics", topics);
+  @Test
+  public void testInitializeKafkaSuccess() {
+    String[] topics = {"test-topic1", "test-topic2"};
+    ReflectionTestUtils.setField(kafkaConfig, "topics", topics);
 
-   kafkaConfig.initializeKafka();
+    kafkaConfig.initializeKafka();
 
-   verify(kafkaAdmin, times(2)).createOrModifyTopics(any(NewTopic.class));
- }
+    verify(kafkaAdmin, times(2)).createOrModifyTopics(any(NewTopic.class));
+  }
 
- @Test
- public void testInitializeKafka_TopicCreationFailure() throws InterruptedException {
-   String[] topics = {"test-topic1"};
-   ReflectionTestUtils.setField(kafkaConfig, "topics", topics);
+  @Test
+  public void testInitializeKafkaTopicCreationFailure() {
+    String[] topics = {"test-topic1"};
+    ReflectionTestUtils.setField(kafkaConfig, "topics", topics);
 
-   doThrow(new RuntimeException("Topic creation failed"))
-           .when(kafkaAdmin)
-           .createOrModifyTopics(any(NewTopic.class));
+    doThrow(new RuntimeException("Topic creation failed"))
+            .when(kafkaAdmin)
+            .createOrModifyTopics(any(NewTopic.class));
 
-   kafkaConfig.initializeKafka();
+    kafkaConfig.initializeKafka();
 
-   verify(kafkaAdmin, times(5)).createOrModifyTopics(any(NewTopic.class));
- }
-
+    verify(kafkaAdmin, times(5)).createOrModifyTopics(any(NewTopic.class));
+  }
 }
