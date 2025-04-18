cd26fe7acf1500f1d26945829af96f8db6f823f4
zlac370 <zlac370@live.rhul.ac.uk>
2025-04-18 10:47:00 +0100

    feat: created a function to get the items from finished auctions

---
+++ product/backend/database/src/sql/functions/get_items_from_finished_auctions.sql
@@ -0,0 +1,33 @@
+-- This function gets the items with finished auctions that haven't been processed
+CREATE OR REPLACE FUNCTION get_items_from_finished_auctions()
+RETURNS TABLE (
+    item_id UUID,
+    name VARCHAR,
+    description TEXT,
+    is_active BOOLEAN,
+    ending_time TIMESTAMP,
+    price DECIMAL,
+    stock INT,
+    category VARCHAR,
+    images JSONB,
+    seller_id UUID
+) AS $$
+BEGIN
+    RETURN QUERY
+    UPDATE items i
+    SET is_active = false
+    WHERE i.ending_time < now()
+      AND i.is_active = true
+    RETURNING
+        i.item_id,
+        i.name,
+        i.description,
+        i.is_active,
+        i.ending_time,
+        i.price,
+        i.stock,
+        i.category,
+        i.images,
+        i.seller_id;
+END;
+$$ LANGUAGE plpgsql;
