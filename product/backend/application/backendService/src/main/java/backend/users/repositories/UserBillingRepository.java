7b5dddd276be4024ca3be6a8d9f081e1cff79182
zlac370 <zlac370@live.rhul.ac.uk>
2025-04-21 01:46:04 +0100

    feat: created user billing repository

---
+++ product/backend/application/backendService/src/main/java/backend/users/repositories/UserBillingRepository.java
@@ -0,0 +1,31 @@
+package backend.users.repositories;
+
+import backend.users.entities.UserBillingEntity;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Repository for user billing entities.
+ */
+public interface UserBillingRepository extends JpaRepository<UserBillingEntity, UUID> {
+
+  /**
+   * This query creates or updates the user billing.
+   *
+   * @param userId     the users billing to update.
+   * @param accountId  the account id.
+   * @param customerId the customer id
+   */
+  @Modifying
+  @Query(value = "SELECT * FROM insert_or_update_user_billing(" +
+          ":userId, :accountId, :customerId)",
+          nativeQuery = true)
+  UserBillingEntity saveUserBilling(
+          @Param("userId") UUID userId,
+          @Param("accountId") String accountId,
+          @Param("customerID") String customerId
+  );
+}
